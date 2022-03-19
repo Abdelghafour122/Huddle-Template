4a642553897ca49fbdfc015343d7dda6f043b40a
Abdelghafour <ghafourragg@gmail.com>
2022-03-19 15:47:40 +0100

    removed fades class and intersectionObserver

--- app/js/script.js
+++ app/js/script.js
@@ -6,16 +6,16 @@ let options = {
   threshold: 0.6,
 };
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    entry.target.classList.toggle("show", entry.isIntersecting);
-    entry.isIntersecting && observer.unobserve(entry.target);
-  });
-}, options);
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     entry.target.classList.toggle("show", entry.isIntersecting);
+//     entry.isIntersecting && observer.unobserve(entry.target);
+//   });
+// }, options);
 
-showerEls.forEach((showerEl) => {
-  observer.observe(showerEl);
-});
+// showerEls.forEach((showerEl) => {
+//   observer.observe(showerEl);
+// });
 
 const EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
